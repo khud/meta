7b5b29c4fd25bf0bf3675bd5cc8591d16bec177a
khud <vitaly.khudobakhshov@gmail.com>
2020-12-08 22:42:05 +0300

    Restore proper name for Op args

--- flowchart/src/main/kotlin/metacourse/flowchart/BuiltIns.kt
+++ flowchart/src/main/kotlin/metacourse/flowchart/BuiltIns.kt
@@ -16,7 +16,7 @@ object BuiltIns {
             if (args.size > 1) throw IllegalArgumentException("$name function accepts only one argument buf found ${args.size}")
             val arg = args[0]
             if (arg is Op && arg.func == "cons") {
-                arg.params[position]
+                arg.args[position]
             } else throw IllegalArgumentException("The only argument should be non empty list but found $arg")
         }
 }
