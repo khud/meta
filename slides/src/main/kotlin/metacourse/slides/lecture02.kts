6baeef47096c62fdab447fb51d91e9d8257d7826
khud <vitaly.khudobakhshov@gmail.com>
2021-01-10 23:06:36 +0300

    Update slides

--- slides/src/main/kotlin/metacourse/slides/lecture02.kts
+++ slides/src/main/kotlin/metacourse/slides/lecture02.kts
@@ -1,14 +1,27 @@
+import io.kotex.beamer.column
+import io.kotex.beamer.columns
 import io.kotex.beamer.frame
 import io.kotex.bibtex.cite
-import io.kotex.core.document
-import io.kotex.core.makeTitle
-import io.kotex.core.textbf
+import io.kotex.core.*
 import metacourse.slides.createPreamble
 import metacourse.slides.mcCarty60
 import java.io.File
 
 val doc = document(createPreamble("Метавычисления")) {
     makeTitle()
+
+    section("История") {
+        frame("Валентин Федорович Турчин") {
+            columns {
+                column(0.6 * textWidth()) {
+                    +"Валентин Фёдорович Турчин (1931 -- 2010)"
+                }
+                column(0.4 * textWidth()) {
+                    includeGraphics("../assets/turchin.jpg", textWidth())
+                }
+            }
+        }
+    }
 
     section("Языки программирования") {
         subsection("Языки типа Lisp") {
@@ -44,4 +57,9 @@ val doc = document(createPreamble("Метавычисления")) {
 //        }
         }
     }
-}
+}.toTex()
+
+val outputDir = File("../../../../../tex/")
+if (!outputDir.exists()) outputDir.mkdirs()
+
+File("$outputDir/lecture02.tex").writeText(doc)
