b97167115ce1f3cfab3d3d2401b8017e207c6218
khud <vitaly.khudobakhshov@gmail.com>
2021-01-12 22:58:36 +0300

    Update slides

--- slides/src/main/kotlin/metacourse/slides/lecture03.kts
+++ slides/src/main/kotlin/metacourse/slides/lecture03.kts
@@ -167,12 +167,66 @@ val doc = document(createPreamble("Язык TSG")) {
     }
 
     section("Семантика языка") {
-        frame("Среда, связи и состояние") {
+        subsection("Среда, связи и состояние") {
 
         }
 
-        frame("Интерпретатор языка") {
+        subsection("Интерпретатор языка") {
+            frame("Функция int") {
+                verbatim("""
+                fun int(prog: ProgR, d: List<Exp>): Exp {
+                  val f = prog.first()
+                  val env = mkEnv(f.params, d)
+                  return eval(State(CALL(f.name, *f.params.toTypedArray()), env), prog)
+                }
+                
+                data class CondRes(val res: Boolean, val env: Env)
+                
+                fun mkEnv(vars: List<Var>, vals: List<Exp>) = 
+                  vars.zip(vals).map { Bind(it.first, it.second) }
 
+                fun getDef(name: FName, prog: ProgR): FDef? = 
+                  prog.find { f -> f.name == name }
+                """)
+            }
+
+            frame("Функция eval") {
+                verbatim("""
+                fun eval(state: State, prog: ProgR): Exp =
+                  when (val term = state.term) {
+                    is Call -> {
+                      val f = getDef(term.name, prog)!!
+                      val env = mkEnv(f.params, term.args / state.env)
+                      eval(State(f.body, env), prog)
+                    }
+                    is Alt -> {
+                      val condRes = cond(term.cond, state.env)
+                      val env = state.env + condRes.env
+                      val next = if (condRes.res) term.then else term.otherwise
+                      eval(State(next, env), prog)
+                    }
+                    is Ret -> term.exp / state.env
+                  }""")
+            }
+
+            frame("Функция cond") {
+                verbatim("""
+                fun cond(cond: Cond, env: Env): CondRes =
+                  when (cond) {
+                    is IsEqa -> {
+                      val x = cond.x / env
+                      val y = cond.y / env
+                      CondRes(x is Atom && y is Atom && x.name == y.name, listOf())
+                    }
+                    is IsCons -> {
+                      when (val x = cond.exp / env) {
+                        is Cons -> CondRes(true, listOf(Bind(cond.head, x.head), 
+                                                        Bind(cond.tail, x.tail)))
+                        is Atom -> CondRes(false, listOf(Bind(cond.atom, x)))
+                        else -> throw IllegalArgumentException(x.toString())
+                      }
+                    }}""")
+            }
         }
     }
 }.toTex()
