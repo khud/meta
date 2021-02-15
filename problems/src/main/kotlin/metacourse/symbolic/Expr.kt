8d8ec2684aa5b09f7e81a0820a9857f3aa39de8a
khud <vitaly.khudobakhshov@gmail.com>
2021-02-15 17:36:32 +0300

    Problem 01

---
+++ problems/src/main/kotlin/metacourse/symbolic/Expr.kt
@@ -0,0 +1,18 @@
+package metacourse.symbolic
+
+sealed class Expr
+data class Value(val value: Double): Expr()
+data class Variable(val name: String): Expr()
+data class Plus(val x: Expr, val y: Expr): Expr()
+data class Minus(val x: Expr, val y: Expr): Expr()
+data class Multiply(val x: Expr, val y: Expr): Expr()
+data class Divide(val x: Expr, val y: Expr): Expr()
+data class Sin(val expr: Expr): Expr()
+
+fun eval(expr: Expr): Expr {
+    TODO()
+}
+
+fun diff(expr: Expr, variable: Variable): Expr {
+    TODO()
+}
